1385edb209a17478928fd2dd18188c998a23be3a
Nastya <161581550+naaaastyaaaaa01@users.noreply.github.com>
2024-04-24 01:43:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def log():
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
-@app.route('/login')
-def login():
-    return render_template('login.html', title='Home')
 def index():
     return redirect(url_for('index'))
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Test')
 if __name__ == '__main__':
     app.run(debug=True)
 
